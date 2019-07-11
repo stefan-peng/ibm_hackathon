384c837ca5ed1f1f26cf7b517daea42248374789
Stefan Peng <pengstefan@outlook.com>
2019-07-10 21:08:10 -0400

    Add login and signup pages

--- src/App.js
+++ src/App.js
@@ -1,41 +1,91 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component, Fragment } from "react";
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Link, withRouter } from 'react-router-dom';
 import './App.css';
 import Routes from "./Routes";
 
-import { Nav, Navbar, NavItem } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap'
-
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isAuthenticated: false,
+      isAuthenticating: true
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      // await // TODO: get current login status
+      // this.userHasAuthenticated(true);
+    }
+    catch(e) {
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+  
+    this.setState({ isAuthenticating: false });
+  }
+
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  }
+
+  handleLogout = event => {
+    this.userHasAuthenticated(false);
+    this.props.history.push("/login");
+    // TODO: logout
+  }
+
   render() {
-  return (
-    <div className="App container">
-      <Navbar fluid collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to="/">Board</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav pullRight>
-            <LinkContainer to="/calendar">
-              <NavItem>Calendar</NavItem>
-            </LinkContainer>
-            <LinkContainer to="/bios">
-              <NavItem>Intern Bios</NavItem>
-            </LinkContainer>
-            <LinkContainer to="/links">
-              <NavItem>Useful links</NavItem>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <Routes />
-    </div>
-  );
-}
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
+
+    return (
+      !this.state.isAuthenticating &&
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">Board</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullLeft>
+              <LinkContainer to="/calendar">
+                <NavItem>Calendar</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/bios">
+                <NavItem>Intern Bios</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/links">
+                <NavItem>Useful links</NavItem>
+              </LinkContainer>
+            </Nav>
+            <Nav pullRight>
+              {this.state.isAuthenticated
+                ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
+                : <Fragment>
+                  <LinkContainer to="/signup">
+                    <NavItem>Signup</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <NavItem>Login</NavItem>
+                  </LinkContainer>
+                </Fragment>
+              }
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes childProps={childProps} />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withRouter(App);
--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import AppliedRoute from "./components/AppliedRoute";
 import Bios from './containers/Bios';
 import Calendar from './containers/Calendar';
 import Home from "./containers/Home";
 import Links from './containers/Links';
+import Login from "./containers/Login";
 import NotFound from "./containers/NotFound";
+import Signup from "./containers/Signup";
 
-
-export default () =>
+export default ({ childProps }) =>
     <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/calendar" exact component={Calendar} />
-        <Route path="/bios" exact component={Bios} />
-        <Route path="/links" exact component={Links} />
-        <Route component={NotFound} />
+        <AppliedRoute path="/" exact component={Home} props={childProps} />
+        <AppliedRoute path="/login" exact component={Login} props={childProps} />
+        <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+        <AppliedRoute path="/calendar" exact component={Calendar} props={childProps} />
+        <AppliedRoute path="/bios" exact component={Bios} props={childProps} />
+        <AppliedRoute path="/links" exact component={Links} props={childProps} />
+        <AppliedRoute component={NotFound} />
     </Switch>;
